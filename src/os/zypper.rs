26aecc4b36c0b855df87d471abef4fa386cebe7e
GavinStrikes <105064040+GavinCoded@users.noreply.github.com>
2026-01-16 17:58:46 -0500

    Add files via upload

---
+++ src/os/zypper.rs
@@ -0,0 +1,14 @@
+use crate::platform;
+use std::sync::mpsc::Sender;
+
+pub fn install(deps: &[&str], log: &Sender<String>) -> Result<(), String> {
+    let _ = log.send("using zypper...".into());
+    let args: Vec<&str> = [&["install", "-y"], deps].concat();
+    platform::run_elevated("zypper", &args)
+}
+
+pub fn uninstall(deps: &[&str], log: &Sender<String>) -> Result<(), String> {
+    let _ = log.send("using zypper...".into());
+    let args: Vec<&str> = [&["remove", "-y"], deps].concat();
+    platform::run_elevated("zypper", &args)
+}
